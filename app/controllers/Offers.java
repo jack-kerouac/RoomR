34634d9ee42d6cbe415f52c238323cc29c42faeb
Florian Rampp <Florian.Rampp@googlemail.com>
2012-02-05 10:52:53 +0100

    offers are not persistet in Twig datastore

--- app/controllers/Offers.java
+++ app/controllers/Offers.java
@@ -1,17 +1,31 @@
 package controllers;
 
+import javax.inject.Inject;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
 import models.Offer;
+import models.OfferRepository;
 import play.data.validation.Valid;
+import play.modules.guice.InjectSupport;
 import play.mvc.Controller;
 
+@InjectSupport
 public class Offers extends Controller {
 
+	@Inject
+	private static OfferRepository offerRepository;
+	
 	public static void createOffer(@Valid Offer offer) {
 		if (validation.hasErrors()) {
 			params.flash(); // add http parameters to the flash scope
 			validation.keep(); // keep the errors for the next request
 			Application.createOffer();
 		}
+		
+		offerRepository.add(offer);
+		
 		renderJSON(offer);
 	}
 }
--- app/models/internal/reposImpl/twig/TwigRepositoryModule.java
+++ app/models/internal/reposImpl/twig/TwigRepositoryModule.java
@@ -1,16 +1,26 @@
 package models.internal.reposImpl.twig;
 
+import play.modules.twig.Datastore;
 import models.OfferRepository;
 import models.seeking.FlatshareSeekerRepository;
 
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.code.twig.ObjectDatastore;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
+import com.google.inject.Provides;
 
 public class TwigRepositoryModule extends AbstractModule {
-	
+
 	@Override
 	protected void configure() {
-		bind(FlatshareSeekerRepository.class).to(TwigFlatshareSeekerRepository.class);
+//		bind(FlatshareSeekerRepository.class).to(TwigFlatshareSeekerRepository.class);
 		bind(OfferRepository.class).to(TwigOfferRepository.class);
 	}
 	
+	@Provides
+	protected ObjectDatastore provideOS() {
+		return Datastore.getInstance();
+	}
 }
