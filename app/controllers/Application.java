4980bd80d66547bbc23d8c07918fe5c3ffe26e28
Thomas Thrainer <thomas.thrainer@jambit.com>
2012-02-06 22:24:32 +0100

    Automatically import development model entities on request to the start page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,10 @@
 package controllers;
 
+import play.mvc.Before;
 import play.mvc.Controller;
-
+import util.DevelopmentModelDataLoader;
 
 public class Application extends Controller {
-
 	public static void start() {
 		render();
 	}
@@ -13,4 +13,11 @@ public class Application extends Controller {
 		render();
 	}
 
+	@Before
+	public static void importDevModels() {
+		// using Plays OnApplicationStart annotation does not work for
+		// GAE-enabled applications, so we load the development data as soon as
+		// we get the first request to the start page
+		DevelopmentModelDataLoader.ensureLoaded();
+	}
 }
