b4dd508a07685e3743b5eda2a95a3eb32d03be95
Thomas Thrainer <thomas.thrainer@jambit.com>
2012-02-12 15:30:13 +0100

    - Modularize JavaScript.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,99 +1,98 @@
 /* Author: Florian Rampp */
 
-$(document).ready(function () {
-	$("#create_request_form").formwizard({
-		formPluginEnabled : false,
-		validationEnabled : false,
-		historyEnabled : true,
-		focusFirstInput : true,
-		formOptions : {
-			success : function (data) {
-				$("#status").fadeTo(500, 1, function () {
-					$(this).html("You are now registered!").fadeTo(5000, 0);
-				});
-			},
-			beforeSubmit : function (data) {
-				$("#data").html("data sent to the server: " + $.param(data));
-			},
-			dataType : 'json',
-			resetForm : true
-		}
-	});
-
-	$(function () {
-		var dates = $("#fromDate, #toDate").datepicker({
-			defaultDate : "+1w",
-			changeMonth : false,
-			numberOfMonths : 2,
-			onSelect : function (selectedDate) {
-				var option = this.id === "fromDate" ? "minDate" : "maxDate", instance = $(this).data("datepicker"), date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
-				dates.not(this).datepicker("option", option, date);
-			}
-		});
-	});
+var roomr = (function() {
+	var my = {};
 	
-	var updateOfferResultList = function() {
+	var modules = [];
+	
+	my.addModule = function(module) {
+		modules.push(module);
+	};
+	
+	my.init = function() {
+		$.each(modules, function(idx, module){
+			module.init();
+		});
+	};
+	
+	return my;
+}());
+
+roomr.instantSearch = (function() {
+	var my = {};
+	
+	function updateOfferResultList() {
 	   $('.offers').load('/offers', $('#search_offers_form').serialize(), function(responseText, textStatus, XMLHttpRequest) {
 		   $('.offer').hide().fadeIn(500);
 	   });
 	}
 	
-	$('#search_offers_form input').each(function() {
-		var typingTimer;               // timer identifier
-		var doneTypingInterval = 200;  // time in ms, 5 second for example
-		   $(this).data('oldVal', $(this).val());
-
-			// start the countdown
-		   $(this).bind("propertychange keyup input paste", function() {
-			   if ($(this).data('oldVal') != $(this).val()) {
-				   $(this).data('oldVal', $(this).val());
-
-			       clearTimeout(typingTimer);
-			       typingTimer = setTimeout(doneTyping, doneTypingInterval);
+	my.init = function() {
+		$('#search_offers_form input').each(function() {
+			var typingTimer;               // timer identifier
+			var doneTypingInterval = 200;  // time in ms, 5 second for example
+			   $(this).data('oldVal', $(this).val());
+	
+				// start the countdown
+			   $(this).bind("propertychange keyup input paste", function() {
+				   if ($(this).data('oldVal') != $(this).val()) {
+					   $(this).data('oldVal', $(this).val());
+	
+				       clearTimeout(typingTimer);
+				       typingTimer = setTimeout(doneTyping, doneTypingInterval);
+				   }
+			   });
+		
+			   // user is "finished typing," do something
+			   function doneTyping() {
+					updateOfferResultList();
 			   }
-		   });
+		});
+		
+		// update list to account for initial content of the field
+		updateOfferResultList();
+	};
 	
-		   // user is "finished typing," do something
-		   function doneTyping() {
-				updateOfferResultList();
-		   }
+	roomr.addModule(my);
+	return my;
+}());
 
-	 });
+roomr.requestForm = (function() {
+	var my = {};
 	
-	// update list to account for initial content of the field
-	updateOfferResultList();
-	 
-});
+	my.init = function() {
+		$("#create_request_form").formwizard({
+			formPluginEnabled : false,
+			validationEnabled : false,
+			historyEnabled : true,
+			focusFirstInput : true,
+			formOptions : {
+				success : function (data) {
+					$("#status").fadeTo(500, 1, function () {
+						$(this).html("You are now registered!").fadeTo(5000, 0);
+					});
+				},
+				beforeSubmit : function (data) {
+					$("#data").html("data sent to the server: " + $.param(data));
+				},
+				dataType : 'json',
+				resetForm : true
+			}
+		});
 
-
-var demo = {
-	common : {
-		init : function () {
-			//initialize
-		},
-		finalize : function () {
-			//finalize
-		},
-		config : {
-			prop : "my value",
-			constant : "42"
-		}
-	},
-	mapping : {
-		init : function() {
-			//create a map
-		},
-		geolocate : function() {
-			//geolocation is cool
-		},
-		geocode : function() {
-			//look up an address or landmark
-		},
-		drawPolylines : function() {
-			//draw some lines on a map
-		},
-		placeMarker : function() {
-			//place markers on the map
-		}
-	}
-}
+		$(function () {
+			var dates = $("#fromDate, #toDate").datepicker({
+				defaultDate : "+1w",
+				changeMonth : false,
+				numberOfMonths : 2,
+				onSelect : function (selectedDate) {
+					var option = this.id === "fromDate" ? "minDate" : "maxDate", instance = $(this).data("datepicker"), date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
+					dates.not(this).datepicker("option", option, date);
+				}
+			});
+		});
+	};
+	
+	roomr.addModule(my);
+	return my;
+}());
