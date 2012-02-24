091bc92803fd82daafad9040dbef3c778840ad29
Florian Rampp <Florian.Rampp@jambit.com>
2012-02-24 17:28:57 +0100

    - added login/logout links on the upper right corner

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,18 +4,10 @@ import play.mvc.Before;
 import play.mvc.Controller;
 import util.DevelopmentModelDataLoader;
 
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-
 public class Application extends Controller {
 	
 	public static void start() {
-        UserService userService = UserServiceFactory.getUserService();
-        String loginUrl = userService.createLoginURL("/");
-        String logoutUrl = userService.createLogoutURL("/");
-        User currentUser = userService.getCurrentUser();
-		render(loginUrl, logoutUrl, currentUser);
+		render();
 	}
 
 	@Before
@@ -25,4 +17,5 @@ public class Application extends Controller {
 		// we get the first request to the start page
 		DevelopmentModelDataLoader.ensureLoaded();
 	}
+	
 }
