eb3178ef79c999f53bb955f12edeae2564cf89ae
Gernot Pointner <Gernot.Pointner@googlemail.com>
2012-03-11 12:19:13 +0100

    - created roomr user repository which uses native google datastore methods

---
+++ app/models/internal/reposImpl/gdsnative/GdsNativeRoomrUserRepository.java
@@ -0,0 +1,36 @@
+package models.internal.reposImpl.gdsnative;
+
+import java.util.Set;
+
+import com.google.appengine.api.users.User;
+
+import models.user.RoomrUser;
+import models.user.RoomrUserRepository;
+
+public class GdsNativeRoomrUserRepository implements RoomrUserRepository {
+
+	@Override
+	public RoomrUser findUser(User gaeUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(RoomrUser newUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<RoomrUser> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(RoomrUser user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
