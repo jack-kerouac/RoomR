ab46c1c683fd25fef036bd0a69311d05b0a49000
Gernot Pointner <Gernot.Pointner@googlemail.com>
2012-05-06 21:22:57 +0200

    - removed obsolete method from application repo interface   ( will be provided by entity itself)

--- app/models/application/RoomOfferApplicationRepository.java
+++ app/models/application/RoomOfferApplicationRepository.java
@@ -2,16 +2,12 @@ package models.application;
 
 import java.util.Set;
 
-import models.offer.RoomOffer;
-
 public interface RoomOfferApplicationRepository {
 
 	public void add(RoomOfferApplication newApplication);
-	
-	public Set<RoomOfferApplication> findAllApplicationsFor(RoomOffer roomOffer);
 
 	public Set<RoomOfferApplication> findAll();
 
 	public void remove(RoomOfferApplication application);
-	
+
 }
