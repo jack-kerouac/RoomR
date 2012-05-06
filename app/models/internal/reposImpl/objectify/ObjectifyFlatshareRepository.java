1bf5526bf2f80a5fecb7b8c4c0d99fcad99be706
Gernot Pointner <Gernot.Pointner@googlemail.com>
2012-05-06 11:19:20 +0200

    [flatshare repository]
    - added flatshare repository interface
    - added objectify repository skeleton
    - bound interface to objectify implementation
    - removed obsolete datastore providing mechanism within roomr module (needed for twig)

---
+++ app/models/internal/reposImpl/objectify/ObjectifyFlatshareRepository.java
@@ -0,0 +1,34 @@
+package models.internal.reposImpl.objectify;
+
+import java.util.Set;
+
+import models.flatshare.Flatshare;
+import models.flatshare.FlatshareRepository;
+
+public class ObjectifyFlatshareRepository implements FlatshareRepository{
+
+	@Override
+	public void add(Flatshare newFlatshare) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<Flatshare> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Flatshare flatshare) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Flatshare flatshare) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
