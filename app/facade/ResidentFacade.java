1906dd9f0c15620f5daf6baa043550740b458b3c
Gernot Pointner <Gernot.Pointner@googlemail.com>
2012-05-13 16:24:00 +0200

    - removed hash check from facade ( is done within controller)

--- app/facade/ResidentFacade.java
+++ app/facade/ResidentFacade.java
@@ -6,7 +6,6 @@ import models.flatshare.Flatshare;
 import models.flatshare.FlatshareRepository;
 import models.offer.RoomOffer;
 import models.offer.RoomOfferRepository;
-import models.offer.RoomOfferTokenService;
 import notifiers.RoomrMailer;
 import facade.exception.RoomOfferUpdateException;
 
@@ -14,7 +13,6 @@ public class ResidentFacade {
 
 	private FlatshareRepository flatshareRepository;
 	private RoomOfferRepository roomOfferRepository;
-	private RoomOfferTokenService roomOfferTokenService;
 
 	public void createFlatshareAndOffer(Flatshare newFlatshare, RoomOffer roomOffer) {
 		flatshareRepository.add(newFlatshare);
@@ -31,27 +29,16 @@ public class ResidentFacade {
 	 *            the offer to update
 	 * @param flatshare
 	 *            the flatshare for which the offer should be updated
-	 * @param hash
-	 *            the associated hash which should be checked for the update
-	 *            process.
 	 * @throws RoomOfferUpdateException
-	 *             when the room offer doesn't belong to the flatshare or the
-	 *             given hash is invalid
+	 *             when the room offer doesn't belong to the flatshare
 	 */
-	public void updateRoomOfferForFlatshare(RoomOffer offer, Flatshare flatshare, String hash)
-			throws RoomOfferUpdateException {
+	public void updateRoomOfferForFlatshare(RoomOffer offer, Flatshare flatshare) throws RoomOfferUpdateException {
 		// check if the offer really belongs to the given flatshare
 		if (flatshare.id != offer.getFlatshare().id) {
 			throw new RoomOfferUpdateException("Room Offer doesn't belong to the given flatshare");
 		}
 
-		// check hash
-		String expectedHash = roomOfferTokenService.createTokenForRoomOffer(offer);
-		if (!expectedHash.equals(hash)) {
-			throw new RoomOfferUpdateException("Incorrect hash supplied for update request");
-		}
-
-		// hash ok -> update
+		// update entities
 		flatshareRepository.update(flatshare);
 		roomOfferRepository.update(offer);
 	}
@@ -65,10 +52,4 @@ public class ResidentFacade {
 	public void setFlatshareRepository(FlatshareRepository flatshareRepository) {
 		this.flatshareRepository = flatshareRepository;
 	}
-
-	@Inject
-	public void setRoomOfferTokenService(RoomOfferTokenService tokenService) {
-		this.roomOfferTokenService = tokenService;
-	}
-
 }
