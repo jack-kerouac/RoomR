f12cffa0492775f38ec29771f4fcfb4763b6f381
Florian Rampp <Florian.Rampp@googlemail.com>
2012-05-13 15:18:40 +0200

    - added the free period to search results

--- app/models/ranking/matching/scoring/StartDateScorer.java
+++ app/models/ranking/matching/scoring/StartDateScorer.java
@@ -8,17 +8,27 @@ import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.joda.time.Interval;
 
+import com.google.common.base.Optional;
+
 public class StartDateScorer extends AbstractAttributeScorer<DateQuery> {
 
 	@Override
 	public Score score(RoomOffer offer, DateQuery startDateQuery) {
 		DateTime roomFreeFrom = new DateTime(offer.roomDetails.freeFrom);
+		Optional<DateTime> roomFreeTo = offer.roomDetails.freeTo == null ? Optional.<DateTime> absent() : Optional
+				.of(new DateTime(offer.roomDetails.freeTo));
 
 		DateTime desiredMovingInDate = new DateTime(startDateQuery.getDate());
 
-		if (roomFreeFrom.isBefore(desiredMovingInDate))
-			return Score.MAX;
-		else {
+		if (roomFreeFrom.isBefore(desiredMovingInDate)) {
+			// if the room is free in the past, it is still free now, otherwise,
+			// the offer would have been removed.
+			// but only, if a potentially defined end date is not in the past
+			if (roomFreeTo.isPresent() && roomFreeTo.get().isBefore(desiredMovingInDate))
+				return Score.MIN;
+			else
+				return Score.MAX;
+		} else {
 			Duration difference = new Interval(desiredMovingInDate, roomFreeFrom).toDuration();
 			if (difference.isShorterThan(Duration.standardDays(14)))
 				return Score.MAX;
@@ -26,8 +36,7 @@ public class StartDateScorer extends AbstractAttributeScorer<DateQuery> {
 				// TODO (Flo): calculate difference here:
 				int daysOver = 14;
 				return Score.defined(-Score.MAX.getValue() / 28.0 * daysOver + Score.MAX.getValue());
-			}
-			else
+			} else
 				return Score.MIN;
 		}
 	}
