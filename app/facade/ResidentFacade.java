baeb5ed1c4940e10dae06e38af2b40453e84f982
Gernot Pointner <Gernot.Pointner@googlemail.com>
2012-05-13 14:34:25 +0200

    - update roomOffer for flatshare
    - implemented method in resident facade
    - created new exception for updating process

--- app/facade/ResidentFacade.java
+++ app/facade/ResidentFacade.java
@@ -1,6 +1,11 @@
 package facade;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.inject.Inject;
+
+import facade.exception.RoomOfferUpdateException;
 
 import models.flatshare.Flatshare;
 import models.flatshare.FlatshareRepository;
@@ -16,6 +21,8 @@ public class ResidentFacade {
 	@Inject
 	private RoomOfferRepository roomOfferRepository;
 
+	public final String SALT = "ROOMR_SECRET";
+
 	public void createFlatshareAndOffer(Flatshare newFlatshare, RoomOffer roomOffer) {
 		flatshareRepository.add(newFlatshare);
 		roomOffer.setFlatshare(newFlatshare);
@@ -23,4 +30,45 @@ public class ResidentFacade {
 		RoomrMailer.offerCreated(roomOffer);
 	}
 
+	/**
+	 * Updates a room offer and its associated flatshare and validates the salt
+	 * value for the update request.
+	 * 
+	 * @param offer
+	 *            the offer to update
+	 * @param flatshare
+	 *            the flatshare for which the offer should be updated
+	 * @param hash
+	 *            the associated hash which should be checked for the update
+	 *            process.
+	 * @throws RoomOfferUpdateException
+	 *             when the room offer doesn't belong to the flatshare or the
+	 *             given hash is invalid
+	 */
+	public void updateRoomOfferForFlatshare(RoomOffer offer, Flatshare flatshare, String hash)
+			throws RoomOfferUpdateException {
+		// check if the offer really belongs to the given flatshare
+		if (flatshare.id != offer.getFlatshare().id) {
+			throw new RoomOfferUpdateException("Room Offer doesn't belong to the given flatshare");
+		}
+
+		// check hash
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RoomOfferUpdateException("Error during hash verification");
+		}
+		String stringToHash = SALT + offer.id;
+		byte[] bytesOfMessage = stringToHash.getBytes();
+		byte[] hashedBytes = md.digest(bytesOfMessage);
+		String hashString = new String(hashedBytes);
+		if (!hashString.equals(hash)) {
+			throw new RoomOfferUpdateException("Incorrect hash supplied for update request");
+		}
+
+		// hash ok -> update
+		flatshareRepository.update(flatshare);
+		roomOfferRepository.update(offer);
+	}
 }
