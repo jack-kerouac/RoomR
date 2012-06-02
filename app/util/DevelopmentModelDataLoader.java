0fe7928c7d4e008ce1e436f1b1a628a6771d569c
Thomas Thrainer <thomas.thrainer@jambit.com>
2012-06-02 18:09:11 +0200

    - use UTF-8 as encoding for development models.

--- app/util/DevelopmentModelDataLoader.java
+++ app/util/DevelopmentModelDataLoader.java
@@ -2,6 +2,7 @@ package util;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -53,7 +54,11 @@ public class DevelopmentModelDataLoader {
 		Yaml yaml = new Yaml(constructor);
 
 		List<Object> all;
-		all = (List<Object>) yaml.load(new InputStreamReader(is));
+		try {
+			all = (List<Object>) yaml.load(new InputStreamReader(is, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
 
 		for (Object o : all) {
 			if (o instanceof RoomrUser)
