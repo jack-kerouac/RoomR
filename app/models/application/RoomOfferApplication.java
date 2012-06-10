39cbef08c84947be6ed83a836ffbe3dc83d6f840
Gernot Pointner <gernot.pointner@googlemail.com>
2012-06-10 17:33:32 +0200

    - added states for room offer application

--- app/models/application/RoomOfferApplication.java
+++ app/models/application/RoomOfferApplication.java
@@ -18,8 +18,7 @@ public class RoomOfferApplication extends ObjectifyModel {
 	Long id;
 
 	public static enum State {
-		// TODO: define useful states
-		WAITING_FOR_INVITATION;
+		WAITING_FOR_INVITATION, INVITED, REJECTED, VISITED, ACCEPTED
 	}
 
 	public State currentState;
