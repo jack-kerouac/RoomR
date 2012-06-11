87311868f59453c8f5262195f12b36df52e88a77
Florian Rampp <Florian.Rampp@googlemail.com>
2012-06-11 21:47:46 +0200

    pretty printing JSON now

--- app/controllers/rest/RoomrGsonBuilder.java
+++ app/controllers/rest/RoomrGsonBuilder.java
@@ -4,6 +4,6 @@ import com.google.gson.GsonBuilder;
 
 public class RoomrGsonBuilder {
 	public static GsonBuilder builder() {
-		return new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		return new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").setPrettyPrinting();
 	}
 }
