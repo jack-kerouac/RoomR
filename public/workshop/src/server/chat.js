44e49aa475784def4a2c23d2d80e52e731f1e6c6
Florian Rampp <Florian.Rampp@googlemail.com>
2012-07-08 12:26:25 +0200

    removed old workshop sources

--- public/workshop/src/server/chat.js
+++
@@ -1,9 +0,0 @@
-var io = require('socket.io').listen(1337);
-
-io.sockets.on('connection', function(socket) {
-  socket.on('message', function(data) {
-  	console.log("Message received");
-    console.log(data);
-    socket.broadcast.send(data);
-  });
-});
